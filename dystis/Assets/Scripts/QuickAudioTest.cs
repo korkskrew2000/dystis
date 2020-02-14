af65e4ca7555551b76557d7a8ac07a3052f88d72
kjtk <kari.knaapi@gmail.com>
2020-02-14 10:35:47 +0200

    AudioFW fixes.

---
+++ dystis/Assets/Scripts/QuickAudioTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickAudioTest : MonoBehaviour {
+    Dictionary<KeyCode, string> bindings = new Dictionary<KeyCode, string>();
+
+    void Start() {
+        bindings.Add(KeyCode.C, "hyppy");
+        bindings.Add(KeyCode.V, "gunshot");
+        bindings.Add(KeyCode.X, "doesntexist");
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        foreach (var kc in bindings.Keys) {
+            if (Input.GetKeyDown(kc))
+                AudioFW.Play(bindings[kc]);
+        }
+    }
+}
