c3c42eb74fc40e53334a452193ba8d819a4e879a
Joni Halttunen <joni.halttunen@gmail.com>
2020-02-20 13:38:47 +0200

    hienosäätöä

--- dystis/Assets/Scripts/GameManager.cs
+++ dystis/Assets/Scripts/GameManager.cs
@@ -29,22 +29,22 @@ public class GameManager : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit)) {
-                Debug.Log(hit.transform.name);
-                if (hit.transform.tag == "NPC") {
-                    Debug.Log(" :: NPC Clicked :: ");
-                    //var c = hit.collider.GetComponent<ITalkable>();
-                    var c = hit.collider.GetComponentInParent<ITalkable>();
-                    if (c != null)
-                        //Debug.Log(" :: c != null :: ");
-                        //c.TalkWith();
-                        hit.transform.parent.GetComponent<NPCController>().TalkWith();
-                }
-            }
-        }
-    }
+   //void Update() {
+   //    if (Input.GetKeyDown(KeyCode.Mouse0)) {
+   //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+   //        RaycastHit hit;
+   //        if (Physics.Raycast(ray, out hit)) {
+   //            Debug.Log(hit.transform.name);
+   //            if (hit.transform.tag == "NPC") {
+   //                Debug.Log(" :: NPC Clicked :: ");
+   //                //var c = hit.collider.GetComponent<ITalkable>();
+   //                var c = hit.collider.GetComponentInParent<ITalkable>();
+   //                if (c != null)
+   //                    //Debug.Log(" :: c != null :: ");
+   //                    //c.TalkWith();
+   //                    hit.transform.parent.GetComponent<NPCController>().TalkWith();
+   //            }
+   //        }
+   //    }
+   //}
 }
