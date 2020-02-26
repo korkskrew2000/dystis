b761a48ec3757131254e6936f37d0d97ac181394
kjtk <kari.knaapi@gmail.com>
2020-02-26 11:13:09 +0200

    Koodin siistimistä, teleporter, fade.

---
+++ dystis/Assets/Scripts/Interactable.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact() {
+        // This method is meant to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    void Update() {
+        if (isFocus && !hasInteracted) {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius) {
+                //Debug.Log("INTERACT");
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+    public void OnFocused(Transform playerTransform) {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused() {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+    private void OnDrawGizmosSelected() {
+        if (interactionTransform == null) {
+            interactionTransform = transform;
+        }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
---
+++ dystis/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Camera cam;
+
+    public Interactable focus;
+
+    void Start() {
+        cam = Camera.main;
+    }
+
+    void Update() {
+
+        if (EventSystem.current.IsPointerOverGameObject()) {
+            return;
+        }
+
+        // If we press left mouse button
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask)) {
+
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+
+                RemoveFocus();
+            }
+        }
+
+        // If we press right mouse button
+        if (Input.GetMouseButtonDown(1)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100)) {
+
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null) {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+
+    void SetFocus(Interactable newFocus) {
+        if (newFocus != focus) {
+            if (focus != null) {
+                focus.OnDefocused();
+            }
+
+            focus = newFocus;
+        }
+
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus() {
+        if (focus != null) {
+            focus.OnDefocused();
+        }
+
+        focus = null;
+    }
+
+}
+
