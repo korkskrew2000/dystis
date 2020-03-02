4b8c4ae624c34048de5eaaad43f8a867cc758988
kjtk <kari.knaapi@gmail.com>
2020-03-02 15:55:56 +0200

    Teleporter fixes, new teleporter.

---
+++ dystis/Assets/Scripts/TeleportActivation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class TeleportActivation : Interactable {
+    
+    public Transform tpDestination;
+
+    public override void Interact() {
+
+        base.Interact();
+
+        TeleportActivate();
+    }
+
+    private void Start() {
+        //Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void TeleportActivate() {
+        Debug.Log("Activating teleport." + transform.name);
+        //bool wasPickedUp = Inventory.instance.Add(item);
+        //if (wasPickedUp) {
+        //    Destroy(gameObject);
+        //}
+        //player.position = tpDestination.position + new Vector3(0, 2, 0);
+        
+    }
+}
--- dystis/Assets/Scripts/Teleporter.cs
+++ dystis/Assets/Scripts/Teleporter.cs
@@ -6,51 +6,86 @@ public class Teleporter : MonoBehaviour {
     [Tooltip("Destination teleporter.")]
     public Transform teleportDestination;
     GameObject player;
-    Camera cam;
     [Tooltip("Longest distance to activate teleport.")]
-    public float teleportActDistance = 2f;
-    public bool teleportInProgress;
-
+    float teleportActDistance = 2f;
+    float teleportFadespeed = 0.5f;
+    bool teleportStarting = false;
+    bool teleportOnGoing = false;
+    bool teleportEnding = false;
     CanvasGroup fadeOverlay;
 
+    Vector3 tpDest;
 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
         fadeOverlay = GameManager.FindObjectOfType<CanvasGroup>();
         //Hide teleporter meshes during runtime
-        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        //gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+        //tpDest = teleportDestination.transform.Find("TeleporterExit").position;
+        tpDest = teleportDestination.transform.position;
+        
+        Debug.Log("START: tpDest name = " + teleportDestination.transform.name);
+
     }
 
     private void Update() {
 
-        if (Input.GetMouseButtonDown(1)) {
+        if (Input.GetMouseButtonDown(0)) {
 
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector3 playerPos = player.transform.position;
-            Vector3 distance = player.transform.position - transform.position;
+            //Vector3 playerPos = player.transform.position;
+            //Vector3 distance = player.transform.position - transform.position;
 
-            if (Physics.Raycast(ray, out hit, teleportActDistance)) {
-                //Debug.DrawLine(ray.origin, hit.point, Color.red, 5f);
-                if (hit.collider != null && (hit.transform.tag == "Teleporter")) {
-                    Debug.Log("We Clicked: " + GetComponentInParent<Transform>().name);
-                    teleportInProgress = true;
-                player.transform.position = teleportDestination.transform.Find("TeleporterExit").position + new Vector3(0, 2, 0);
-                }
-            }
+            if (Physics.Raycast(ray, out hit, teleportActDistance) && hit.collider != null && hit.transform.tag == "Teleporter") {
+                Debug.DrawLine(ray.origin, hit.point, Color.red, 5f);
+                //if (hit.collider != null && hit.transform.tag == "Teleporter") {
+                    Debug.Log("We Clicked: " + hit.transform.name);
+                    //teleportStarting = true;
 
-            if (teleportInProgress) {
-                AudioFW.Play("teleport");
-                fadeOverlay.alpha = (fadeOverlay.alpha + (Time.deltaTime * 0.1f));
-                //player.transform.position = teleportDestination.transform.Find("TeleporterExit").position + new Vector3(0, 2, 0);
+                    Debug.Log("Teleport destination:" + teleportDestination.name);
+                    //Debug.Log("Teleport destination:" + hit.transform.GetComponentInParent<Transform>().Find("TeleporterExit").GetComponent<Transform>().name);
+                    player.transform.position = tpDest + new Vector3(0, 2, 0);
 
-                if (fadeOverlay.alpha == 1f) // when the screen is fully Black
-                {
-                    teleportInProgress = false;
-                }
+                //}
             }
 
         }
+
+        //    if (teleportStarting) {
+        //        fadeOverlay.alpha += Time.deltaTime * teleportFadespeed;
+        //        Debug.Log("Faderoverlay Alpha: " + fadeOverlay.alpha);
+        //        if (fadeOverlay.alpha >= 1f) // when the screen is fully Black
+        //        {
+        //            teleportStarting = false;
+        //            teleportOnGoing = true;
+        //            teleportEnding = false;
+
+        //            //Debug.Log("Faderoverlay Alpha: " + fadeOverlay.alpha);
+        //        }
+        //    }
+
+        //    if(teleportOnGoing) {
+        //        Debug.Log("Teleport destination:" + teleportDestination.name);
+        //        AudioFW.Play("teleport");
+        //        tpDest = teleportDestination.transform.Find("TeleporterExit").position;
+        //        player.transform.position = tpDest + new Vector3(0, 2, 0);
+        //        //player.transform.position = teleportDestination.transform.fin
+        //        teleportStarting = false;
+        //        teleportOnGoing = false;
+        //        teleportEnding = true;
+        //    }
+
+        //    if (teleportEnding) {
+        //        fadeOverlay.alpha -= Time.deltaTime * teleportFadespeed;
+        //        if (fadeOverlay.alpha <= 0f)
+        //        {
+        //            teleportStarting = false;
+        //            teleportOnGoing = false;
+        //            teleportEnding = false;
+        //        }
+        //    }
     }
 
 }
