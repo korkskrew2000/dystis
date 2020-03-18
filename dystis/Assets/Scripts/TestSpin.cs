deb13e86c284f7c37be20799e5dcd9b5f76f0590
xyberspace-drifter <lyyrakimi@gmail.com>
2020-03-18 23:24:08 +0200

    Ilmastointi kone

---
+++ dystis/Assets/Scripts/TestSpin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpin : MonoBehaviour
+{
+    public float spinSpeed = 1f;
+
+    void Update()
+    {
+        transform.Rotate (spinSpeed,0,0 * Time.deltaTime);
+    }
+}
