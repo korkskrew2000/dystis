e0ba3dfe932797178e6b58edf85fed8ab7f07a34
kjtk <kari.knaapi@gmail.com>
2020-04-07 16:15:16 +0300

    MapManager test.

---
+++ dystis/Assets/Scripts/MapManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public Camera MapCamera;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MapCamera.transform.position = player.transform.position;
+    }
+}
