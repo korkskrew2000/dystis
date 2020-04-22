33c27d8a1e778f91d647a1cbdf068203f842e6cf
jonihalt <joni.halttunen@gmail.com>
2020-04-22 05:57:29 +0300

    questit melkein toimii

---
+++ dystis/Assets/Scripts/ChoiceEvents/GiveItemByTalk.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveItemByTalk : ChoiceEvent
+{
+    public Item itemToGive;
+
+    public override void OnMadeChoice(string questionID, int choiceID)
+    {
+        if (questionID == questKeyword && choiceID == questAcceptingChoice)
+        {
+            Inventory.instance.Add(itemToGive);
+        }
+    }
+}
